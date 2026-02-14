2b5a10a676440a72121cc043d6aecfb71bc51b0b
Maple <wjxa20152015@gmail.com>
2026-02-14 09:41:42 +0800

    mv

--- labeling_tool/main.py
+++ labeling_tool/main.py
@@ -2,8 +2,13 @@ import sys
 from PySide2.QtWidgets import QApplication
 from labeling_tool.gui import LabelingTool
 
-if __name__ == "__main__":
+
+def main():
     app = QApplication(sys.argv)
     window = LabelingTool()
     window.show()
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
